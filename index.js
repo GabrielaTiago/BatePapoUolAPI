3942f98ae79ef37c0a5f8ac77fbfc9c2f349f6f8
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2022-06-23 12:40:57 -0300

    Feat: Connecting MongoDB Database to Project

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import {MongoClient, mongoClient} from "mongodb";
 
 
 dotenv.config();
 
+const mongoClient = new MongoClient(MONGO_URI);
+let db;
+
+mongoClient.connect().then(() => {
+    db = mongoClient.db("Bate Papo UOL");
+});
 const server = express(process.env.MONGO_URI);
 
 server.listen(5000);
