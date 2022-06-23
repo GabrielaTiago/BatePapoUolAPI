fee60f76fef35b79de4e75a5eabfa759a8426690
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2022-06-23 12:33:55 -0300

    Feat: Creating the project's .env environment

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
 
-const server = express();
+
+dotenv.config();
+
+const server = express(process.env.MONGO_URI);
 
 server.listen(5000);
