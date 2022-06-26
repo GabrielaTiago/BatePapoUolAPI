95f2e6f5968fc85106dca7ba35269831aecb05c9
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2022-06-26 16:23:29 -0300

    Feat: Created the post route of participants

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-import express from "express";
+import express, { json } from "express";
 import dotenv from "dotenv";
-import {MongoClient, mongoClient} from "mongodb";
-
+import { MongoClient } from "mongodb";
+import cors from "cors";
 
 dotenv.config();
 
-const mongoClient = new MongoClient(MONGO_URI);
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
 mongoClient.connect().then(() => {
-    db = mongoClient.db("Bate Papo UOL");
+    db = mongoClient.db("BatePapoUOL");
 });
+
 const server = express(process.env.MONGO_URI);
+
+server.use(json());
+server.use(cors());
+
+
+server.post("/participants", async (require, response) => {
+    const { name } = require.body;
+
+    try {
+        await db
+            .collection("participants")
+            .insertOne({ name: name, lastStatus: Date.now() });
+
+        response.status(201).send("ok");
+    } catch (error) {
+        console.error(error);
+        response.status(422).send(error);
+    }
+});
 
 server.listen(5000);
